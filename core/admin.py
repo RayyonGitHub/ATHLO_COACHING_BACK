f2d4bf30005f69beaa4f593e44b39907f4febf01
= <y.ferhat@esi-sba.dz>
2026-01-26 08:23:12 +0100

    minor update

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Coach, Client
-admin.site.register(Client)
 # Register your models here.
 admin.site.register(Coach)
 admin.site.register(Client)
