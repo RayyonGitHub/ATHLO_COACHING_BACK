baf150fa313097b127983d8f44e650e6fbed46de
MeryemCevik <mcevik6738@gmail.com>
2026-03-21 22:37:27 +0100

    page prospect et gestion auth coach

--- archiweb_back/settings.py
+++ archiweb_back/settings.py
@@ -127,3 +127,15 @@ SIMPLE_JWT = {
 LOGIN_REDIRECT_URL = '/api/exercices/'
 LOGOUT_REDIRECT_URL = '/api/exercices/'
 CORS_ALLOW_ALL_ORIGINS = True
+
+# --- CONFIG EMAIL ---
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'mcevik6738@gmail.com'
+EMAIL_HOST_PASSWORD = 'quyq ctlw robd kzgc'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
