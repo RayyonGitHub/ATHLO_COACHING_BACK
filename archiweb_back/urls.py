a091896249b59c3df02a0735f4c9da5b284da542
RayyonGitHub <rayanebahmed@gmail.com>
2026-05-13 12:52:56 +0200

    planning

--- archiweb_back/urls.py
+++ archiweb_back/urls.py
@@ -8,7 +8,7 @@ from core.views_shop import ProduitViewSet, CategorieProduitViewSet, CreateShopP
 from core import views
 from core.views_nutrition import RecetteViewSet, PlanNutritionnelViewSet
 from core.views import CreateOrderView
-from core.views_responsable import ResponsableDashboardStatsView
+from core.views_responsable import ResponsableDashboardStatsView, ResponsablePlanningView
 # core/urls.py
 # Ajoutez AthleteMyPlansView (ou le nom exact de votre vue)
 from core.views_nutrition import RecetteViewSet, PlanNutritionnelViewSet
@@ -189,8 +189,9 @@ path('api/admin/me/change-password/', admin_change_my_password, name='admin-chan
     path('api/shop/create-intent/', CreateShopPaymentIntentView.as_view(), name='shop-create-intent'),
     path('api/stripe/webhook/', stripe_webhook, name='stripe-webhook'),
     
-    # Dashboard Responsable Salle
+   # Dashboard Responsable Salle
     path('api/responsable/dashboard-stats/', ResponsableDashboardStatsView.as_view(), name='responsable-dashboard-stats'),
+    path('api/responsable/planning/', ResponsablePlanningView.as_view(), name='responsable-planning'),
 ]
 
 if settings.DEBUG:
